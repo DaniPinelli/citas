ec5586dbd66c90cb3e3ab70888b9271dabc31516
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-02-11 16:56:48 -0300

    Recibiendo props del app y seteando estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
@@ -6,11 +7,13 @@ import ListadoPacientes from './components/ListadoPacientes'
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([]);
+
   return (
     <div className="container mx-auto mt-10" >
       <Header />
       <div className="mt-10 md:flex" >
-        <Formulario />
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} />
         <ListadoPacientes />
       </div>
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,6 @@
 import { useState, setstate } from 'react';
 
-const Formulario = () => {
+const Formulario = ({ pacientes, setPacientes }) => {
 
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
@@ -8,14 +8,35 @@ const Formulario = () => {
     const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
+    const [error, setError] = useState(false);
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if ([nombre, propietario, email, fecha, sintomas].includes('')) {
-            alert('Todos los campos son obligatorios');
-        } else {
-            alert('Enviando formulario');
+
+
+            setError(true)
+            return;
         }
+
+        setError(false)
+
+        const objetoPaciente = {
+            nombre,
+            propietario,
+            email,
+            fecha,
+            sintomas
+        }
+
+        setPacientes([...pacientes, objetoPaciente])
+
+        setNombre('')
+        setPropietario('')
+        setEmail('')
+        setFecha('')
+        setSintomas('')
     }
 
 
@@ -29,6 +50,9 @@ const Formulario = () => {
             </p>
 
             <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-2 px-5" >
+
+
+                {error && <div> <p className="text-red-600 text-center uppercase font-bold mt-2" >Todos los campos son obligatorios</p></div>}
                 <div className="mt-5" >
                     <label className="block text-gray-800 uppercase font-bold" htmlFor="mascota" >Nombre Mascota</label>
                     <input value={nombre} onChange={(e) => setNombre(e.target.value)} id="mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md" type="text" placeholder="Nombre de mascota" />
